27ccf9d003122412f8ea2ada6f26e5a5b020c803
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-19 12:09:33 +0300

    Update app.js

--- src/ js/app.js
+++ src/ js/app.js
@@ -1,14 +1,14 @@
 export default class ErrorRepository {
-    constructor(){
-        this.errors = new Map([
-            [1, 'Error1']
-        ])
+  constructor() {
+    this.errors = new Map([
+      [1, "Error1"],
+    ]);
+  }
+
+  translate(code) {
+    if (!this.errors.has(code)) {
+      throw new Error("Unknown error");
     }
-    translate(code){
-        if(!this.errors.has(code)){
-            throw new Error('Unknow error');
-        }else {
-            return this.errors.get(code);
-        }
-    }
+    return this.errors.get(code);
+  }
 }
